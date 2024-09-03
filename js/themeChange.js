7a37aab18a7941b28def70a2e143e76b86b728ee
carinadaniluk <164891027+carinadaniluk@users.noreply.github.com>
2024-09-02 23:01:47 -0400

    Update themeChange.js
    adding js w3 schools scroll to top

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,15 +1,19 @@
-console.log("Your index.js file is loaded correctly");
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $("#topArrow").fadeIn(200);         // Fade in the arrow
-    } else {
-        $("#topArrow").fadeOut(200);   // Else fade out the arrow
-    }
-});
-$("#topArrow").click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
